cff8a9ae289ef7af78aea07c7e0a555c0a221c05
mukesh_kumar <mukesh.webethics@gmail.com>
2024-04-05 11:34:50 +0530

    working on dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,21 @@ mobiletoggle.addEventListener("click", () => {
     sidebarBody.classList.toggle("sidebar-toggle")
     console.log("clicking")
 })
+
+
+
+
+// const sidebar = document.querySelector('.sidebar-bodyr');
+
+// function openSidebar() {
+//     sidebar.classList.add('slideXanimation');
+//     sidebar.style.transform = 'scaleX(1)';
+// }
+
+// function closeSidebar() {
+//     sidebar.classList.add('slideXanimation');
+//     sidebar.style.transform = 'scaleX(0)';
+//     setTimeout(() => {
+//         sidebar.style.display = 'none';     // Hide sidebar after animation completes
+//     }, 500);   //     Adjust this value to match the animation duration
+// }
