0c9109fe0f1d4ed1773381b33b4aa4546b38ded1
mukesh_kumar <mukesh.webethics@gmail.com>
2024-04-09 17:41:27 +0530

    working on add new program page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,8 @@
 const hamburger = document.querySelector(".hamburger")
 const sidebarBody = document.querySelector(".sidebar-body")
 const mobiletoggle = document.querySelector(".mobile-toggle")
+// const datepicker = document.querySelector("#datepicker")
+
 hamburger.addEventListener("click", () => {
     sidebarBody.classList.toggle("sidebar-toggle")
     console.log("clicking")
@@ -15,17 +17,19 @@ mobiletoggle.addEventListener("click", () => {
 
 
 
-// const sidebar = document.querySelector('.sidebar-bodyr');
 
-// function openSidebar() {
-//     sidebar.classList.add('slideXanimation');
-//     sidebar.style.transform = 'scaleX(1)';
-// }
+// $(function () {
+//     $('#datepicker').datepicker();
+//     console.log("input clicked")
+// });
 
-// function closeSidebar() {
-//     sidebar.classList.add('slideXanimation');
-//     sidebar.style.transform = 'scaleX(0)';
-//     setTimeout(() => {
-//         sidebar.style.display = 'none';     // Hide sidebar after animation completes
-//     }, 500);   //     Adjust this value to match the animation duration
-// }
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+//     var datepicker = document.getElementById('datepicker');
+//     datepicker.addEventListener('click', function () {
+//         console.log("input clicked");
+//     });
+
+// });
